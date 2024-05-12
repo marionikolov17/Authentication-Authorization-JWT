6ffa976dd27e75c9ba633d33a52288c292db3ce0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 13:17:19 +0300

    Create login route in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,24 @@ const router = require("express").Router();
 const authService = require("./../services/authService");
 
 router.post("/login", async (req, res) => {
-    await authService.loginUser(req.body);
+    try {
+        const token = await authService.loginUser(req.body);
 
-    res.send("Login");
+        res.cookie("auth", token);
+        res.status(200).json({
+            status: "success",
+            data: {
+                token
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            data: {
+                error: err.message
+            }
+        })
+    }
 });
 
 module.exports = router;
