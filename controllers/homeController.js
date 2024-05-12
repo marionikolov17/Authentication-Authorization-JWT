1b287cf4178c9ba4e16ce42504d57f3a8581ecb9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 13:35:02 +0300

    Create test route in homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 
 router.get("/test", (req, res) => {
-    
+    res.status(200).json({
+        status: "success",
+        data: {
+            message: "You are logged in."
+        }
+    });
 });
 
 router.get("/coach", (req, res) => {
-
+    
 });
 
 module.exports = router;
