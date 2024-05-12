90dceda31ea736367c28ac57cbaab89402078bf2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 12:40:32 +0300

    Create dummy database

---
+++ data/database.js
@@ -0,0 +1,16 @@
+const db = [
+    {
+        id: 1,
+        username: "mario",
+        password: "1234",
+        role: "coach"
+    },
+    {
+        id: 2,
+        username: "ivan",
+        password: "1234",
+        role: "trainee"
+    }
+]
+
+module.exports = db;
