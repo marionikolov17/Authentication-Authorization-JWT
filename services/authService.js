fa45bb7f59249066c7dc6cf17f98d0ed0263b015
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 13:03:45 +0300

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,8 @@
 const jwt = require("./../lib/jwt");
 const SECRET = require("./../config/secret");
 
+const db = require("./../data/database");
+
 const generateToken = async (user) => {
     const payload = {
         id: user.id,
@@ -9,4 +11,14 @@ const generateToken = async (user) => {
 
     const token = await jwt.sign(payload, SECRET);
     return token;
+}
+
+const loginUser = async (data) => {
+    const user = db.find((obj) => obj.username === data.username);
+
+    console.log(user);
+}
+
+module.exports = {
+    loginUser
 }
