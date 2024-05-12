70e69fd5fe796386525b5c3974be4d99ebe8a826
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 13:05:22 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+const authService = require("./../services/authService");
+
+router.post("/login", async (req, res) => {
+    await authService.loginUser(req.body);
+
+    res.send("Login");
+});
+
 module.exports = router;
