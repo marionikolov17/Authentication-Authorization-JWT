2707849936cf0b2286a4171c2207a88505ba112d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 13:20:01 +0300

    Import homeController in routes

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const authController = require("./controllers/authController");
+const homeController = require("./controllers/homeController");
 
 router.use("/auth", authController);
+router.use("/home", homeController);
 
 module.exports = router;
