a2dc9789a5026fb8f75045f6f88861c77c87573d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-12 12:56:00 +0300

    Update expressConfig.js

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -1,6 +1,8 @@
+const express = require("express");
 const cookieParser = require("cookie-parser");
 
 function expressConfig(app) {
+    app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     return app;
 }
