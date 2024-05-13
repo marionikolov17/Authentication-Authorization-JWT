0a94471a2e49180c79c27765115a0029f2571e9a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-13 18:45:36 +0300

    Update expressConfig.ts

--- src/config/expressConfig.ts
+++ src/config/expressConfig.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
-export default function expressConfig(app: any) {
+export default function expressConfig(app: express.Application) {
     app.use(express.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(cookieParser());
