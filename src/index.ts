675abfdd8b2139f8c0bc018d059569447a9335e4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-21 22:11:04 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 
+//TEST
+import User from "./database/models/users";
+
 // Config imports
 import expressConfig from "./config/expressConfig";
 
@@ -14,11 +17,22 @@ const app = express();
 // Express config
 expressConfig(app);
 
+app.get("/test", async (req: any, res: any) => {
+    try {
+        await User.create({ name: "ok", email: "ok" });
+        return res.send("Success");
+    } catch (err) {
+        return res.send("error");
+    }
+})
+
 // Auth middleware - first middleware
 app.use(authMiddleware);
 
 // App router
 app.use(router);
+
+
 
 const port = 3000;
 
